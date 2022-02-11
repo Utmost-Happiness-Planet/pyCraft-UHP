10391a14a71d947add91b0278c83b6f1446a3403
Kl1nge5 <2351859943@qq.com>
2022-02-11 14:39:46 +0800

    í³æ·»å ææåçå¾ææ¡£

--- minecraft/schematic/__init__.py
+++ minecraft/schematic/__init__.py
@@ -4,4 +4,14 @@ from nbt import nbt
 class Schematic:
     def __init__(self, schematic_path: str):
         self.schematic = nbt.NBTFile(schematic_path)
-        print(self.schematic)
+        self.size = self.schematic[3][0][4]
+        self.sizeX = self.size[0].value
+        self.sizeY = self.size[1].value
+        self.sizeZ = self.size[2].value
+        self.totalVolume = self.sizeX * self.sizeY * self.sizeZ
+        self.sizeLayer = self.sizeX * self.sizeZ
+
+        self.bitArray = ""
+
+    def getIndex(self, x: int, y: int, z: int):
+        return (y * self.sizeLayer) + z * self.sizeX + x
--- tests/schematic/test_nbtinfo.py
+++ tests/schematic/test_nbtinfo.py
@@ -1,4 +1,4 @@
 from minecraft.schematic import Schematic
 
 if __name__ == '__main__':
-    Schematic("4block.litematic")
+    s = Schematic("Colorful.litematic")
